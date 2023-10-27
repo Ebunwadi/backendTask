07abc272ce927ace5a2d01a822f2fea2c5ce229e
Ebube Nwadiokwu <111129501+Ebunwadi@users.noreply.github.com>
2023-10-27 14:32:46 +0100

    Update rateLimiter.ts

--- src/config/rateLimiter.ts
+++ src/config/rateLimiter.ts
@@ -2,7 +2,7 @@ import rateLimit from 'express-rate-limit'
 
 export const limiter = rateLimit({
   windowMs: 60 * 1000, // 1 minute
-  max: 100, // Limit each IP to 5 requests per `window` per minute during production
+  max: 5, // Limit each IP to 5 requests per `window` per minute during production
   handler: (_, res) =>
     res.status(429).json({
       error: { message: 'Too many request attempts from this IP, please try again after a 60 second pause' }
